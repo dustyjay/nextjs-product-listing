a223c5701b5e13e2c62bc347db58097d1a69e698
Janet <janetarisah01@gmail.com>
2024-10-23 23:32:33 +0100

    fix: deployment fail

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['fakestoreapi.com'], unoptimized: true }
+  images: { domains: ['fakestoreapi.com'], unoptimized: true },
+  distDir: 'dist'
 }
 
 module.exports = nextConfig
