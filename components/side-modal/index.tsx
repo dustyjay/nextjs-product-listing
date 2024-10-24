06ffd5658156e9873aefc3963e79b1c63fa6ffc0
Janet <janetarisah01@gmail.com>
2024-10-24 11:20:42 +0100

    fix: cursor on modal overlay should not look interactive

--- components/side-modal/index.tsx
+++ components/side-modal/index.tsx
@@ -12,7 +12,7 @@ const SideModal = ({ children, title, onClose }: Props) => {
       <div
         role='button'
         onClick={onClose}
-        className='fixed bg-opacity-15 bg-gray-400 w-screen h-screen top-0 left-0'></div>
+        className='fixed bg-opacity-15 bg-gray-400 w-screen h-screen top-0 left-0 cursor-auto'></div>
       <div className='fixed flex flex-col right-0 top-0 w-full md:w-4/5 max-w-[500px] bg-white border-l h-screen py-10'>
         <div className='flex gap-6 items-start justify-between px-6 mb-16'>
           <h3>{title}</h3>
